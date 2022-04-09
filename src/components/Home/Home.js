437964c4cbe9147ff993c831230b374e38d5cc65
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-04-09 10:00:37 -0700

    added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Home = () => {
+    const {user} = useFirebase();
     return (
         <div>
             <h2>This is home page</h2>
+            <p>current user is : {user? user.displayName : 'no body'}</p>
         </div>
     );
 };
